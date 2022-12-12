1168ca418f55f16927b399e9439de4469546fa4e
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2022-12-12 14:29:26 +0100

    test disgn

---
+++ testTailWind.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="dist/output.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<!-- Pin to bottom right corner -->
+<div class="relative h-32 w-32 hover:cursor-alias ...">
+  <div class="absolute bottom-0 left-0 h-16 w-16 ">09</div>
+</div>
+</body>
+</html>
