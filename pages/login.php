c389269da2a07ae418fddb72573583e5672a1f5c
Titouan Pastor <titouan.pastor@gmail.com>
2022-12-16 12:13:55 +0100

    Modification de la page de login (style)

--- pages/login.php
+++ pages/login.php
@@ -13,26 +13,25 @@
     <main class="w-screen h-screen flex items-center justify-center">
         <section class="flex w-[800px] h-96 border-slate-200 border-[1px] rounded">
             <div class="flex flex-col items-center text-center justify-center w-1/2 h-full bg-gradient-to-br from-violet-700 to-violet-900">
-                <img class="w-32" src="../img/team-logo.png" alt="Logo United">
+                <img class="w-44" src="../img/team-logo.png" alt="Logo United">
                 <span class="text-white text-3xl">United Chasetag</span>
             </div>
-            <div>
-                <form action="" class="w-full h-full">
-                    <div class="flex flex-col items-center justify-center h-full">
-                        <div class="flex flex-col w-1/2">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" id="email" class="border-slate-200 border-[1px] rounded p-2">
-                        </div>
-                        <div class="flex flex-col w-1/2">
-                            <label for="password">Mot de passe</label>
-                            <input type="password" name="password" id="password" class="border-slate-200 border-[1px] rounded p-2">
-                        </div>
-                        <div class="flex flex-col w-1/2">
-                            <button type="submit" class="w-fit bg-violet-700 transition-colors p-2 rounded hover:bg-violet-800">Se connecter</button>
-                        </div>
+            <form action="login.php" method="POST" class="flex flex-col items-center justify-center w-full h-full pt-10">
+                    <h1 class="text-4xl font-medium">Se connecter</h1>
+                <div class="flex flex-col items-center justify-center w-full h-full">
+                    <div class="flex flex-col">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" class="border-slate-200 border-[1px] rounded p-2 w-60">
                     </div>
-                </form>
-            </div>
+                    <div class="flex flex-col pt-4">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" name="password" id="password" class="border-slate-200 border-[1px] rounded p-2 w-60">
+                    </div>
+                    <div class="flex flex-col pt-10">
+                        <button type="submit" class="w-fit bg-violet-700 transition-colors p-2 rounded hover:bg-violet-800 text-white text-lg">Se connecter</button>
+                    </div>
+                </div>
+            </form>
         </section>
     </main>
 </body>
