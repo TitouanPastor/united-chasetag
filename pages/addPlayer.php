49b912dc57aee7cfb8fb56f1071c12943ab59daa
Baptiste Bayche <baptiste.bayche@etu.iut-tlse3.fr>
2023-01-06 10:41:18 +0100

    correction bug upload

--- pages/addPlayer.php
+++ pages/addPlayer.php
@@ -119,7 +119,7 @@ if(isset($_POST["return"])){
                             <label class="block uppercase  tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-picture">
                                 Photo
                             </label>
-                            <input class="w-full bg-gray-200 text-gray-700 border border-purple-800 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-picture" name="picture" type="file" placeholder="Lien vers votre photo" accept="image/png, image/jpeg value="<?php echo $picture; ?>">
+                            <input class="w-full bg-gray-200 text-gray-700 border border-purple-800 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-picture" name="picture" type="file" placeholder="Lien vers votre photo" accept="image/png, image/jpeg" value="<?php echo $picture; ?>">
                         </div>
 
                     </div>
